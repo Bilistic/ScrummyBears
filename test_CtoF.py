3517f2ce52d1da24bc14ea5b14f425eb216e1eaa
Bilistic <joshua.nuttall@mycit.ie>
2018-04-24 10:13:12 +0100

    Update test_CtoF.py

--- test_CtoF.py
+++ test_CtoF.py
@@ -1,9 +1,7 @@
 import CtoF
 
 def test_CtoF():
-    val = 1
-    if assert CtoF.CtoF(10) == 50:
-        val = 0
-    if assert CtoF.CtoF(11) == 55.8:
-        val =  1
-    return val
+    assert CtoF.CtoF(10) == 50
+ 
+def test_two():
+    assert CtoF.CtoF(11) == 55.8
